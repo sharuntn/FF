37fb1169617cd1440227bd291a7c40861752c1b7
Sharun T N <sharuntn5@gmail.com>
2023-12-05 19:07:19 +0530

    added AI food recoganisation

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
 from base.forms import ImageForm
+from .mingo import *
+from PIL import Image
+from django.core.files.storage import FileSystemStorage
+
 #from mingo import *
 # Create your views here.
 def loginPage(request):
@@ -17,16 +21,22 @@ def scan(request):
         if form.is_valid():
             form.save()
             img_obj = form.instance
+
             return render(request,'base/scan.html',{'form':form,'img_obj':img_obj})
     else:
         form = ImageForm()
         return render(request,'base/scan.html', {'form': form})
-
+    
+    
 def result(request):
+    print(request.POST)
     if request.method == 'POST':
         form = ImageForm(request.POST,request.FILES)
         if form.is_valid():
                 form.save()
                 img_obj = form.instance
-        return render(request,'base/result.html',{'form':form,'img_obj':img_obj})
+                value = prediction(img_obj.image)
+                
+        return render(request,'base/result.html',{'form':form,'img_obj':img_obj,'value':value})
    
+
